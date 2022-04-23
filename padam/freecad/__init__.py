bcf702ac3cc4c075197b23c5549bda1c5856f2de
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2022-04-23 11:51:09 +0200

    add freecad import machinery

---
+++ padam/freecad/__init__.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+import sys
+from importlib import import_module
+from pathlib import Path
+from types import ModuleType
+
+
+FREECAD_MODULES = [
+    'FreeCAD'
+]
+
+
+def _add_freecad_to_sys_path():
+    for path in _get_freecad_paths():
+        sys.path.append(path)
+
+
+def _get_freecad_paths():
+    freecad_command = os.getenv('FREECAD_COMMAND', 'freecadcmd')
+    script_path = Path(__file__).parent / '_get_paths.py'
+    output = subprocess.check_output([freecad_command, script_path])
+    paths = []
+    for line in output.splitlines():
+        if line == b'__end__':
+            break
+        path = line.decode('utf8')
+        if Path(path).exists() and path not in sys.path:
+            paths.append(path)
+    return paths
+
+
+try:
+    _add_freecad_to_sys_path()
+    for module in FREECAD_MODULES:
+        globals()[module] = import_module(module)
+    freecad_missing = False
+except:
+    freecad_missing = True
+    for module in FREECAD_MODULES:
+        globals()[module] = ModuleType('FreeCADFake')
+
+
+if __name__ == '__main__':
+    print(_get_freecad_paths())
