c845e840f5d5afe42549d916b95a0b705c1783b8
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2023-12-08 23:26:09 +0100

    add support for hex colors

--- padam/constants.py
+++ padam/constants.py
@@ -1,4 +1,10 @@
 MATERIAL_COLOR = dict(
-    plywood=[0.9, 0.8, 0.6],
-    hardwood=[0.65, 0.5, 0.4],
+    plywood='#f8f2dc',
+    hardwood='#cba791',
 )
+
+for key, value in MATERIAL_COLOR.items():
+    if isinstance(value, str):
+        hex_string = value.lstrip('#')
+        color = [int(hex_string[i:i+2], 16) / 256 for i in (0, 2, 4)]
+        MATERIAL_COLOR[key] = color
